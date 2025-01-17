2dd65bb5bec487fe3540a08e353cf8c0e9b43ac8
archdsp <archdsp01@gmail.com>
2025-01-17 22:29:17 +0900

    WIP: working on loss increase problem

---
+++ train.py
@@ -0,0 +1,141 @@
+import math
+import numpy as np
+import torch
+from torchvision import transforms
+from torch.utils.data import DataLoader, sampler
+from dataset import COCODataset
+from vocabulary import Vocabulary
+from tqdm import tqdm
+from model import Encoder, Decoder, ImageCaption
+import matplotlib.pyplot as plt
+import torchvision.transforms as transforms
+from PIL import Image
+
+
+def show_image_with_caption(image_tensor, caption_tensor, vocab):
+    """
+    이미지와 캡션을 시각화하는 함수.
+
+    Args:
+        image_tensor: torch.Tensor, 이미지 텐서 (shape: [3, H, W])
+        caption_tensor: torch.Tensor, 캡션 텐서 (정수 인덱스)
+        vocab: Vocabulary 객체, 정수 인덱스를 단어로 변환하는 데 사용
+    """
+    # 1. 이미지 텐서를 복원
+    unnormalize = transforms.Compose([
+        transforms.Normalize(mean=[-0.485 / 0.229, -0.456 / 0.224, -0.406 / 0.225],
+                             std=[1 / 0.229, 1 / 0.224, 1 / 0.225]),
+        transforms.ToPILImage()  # 텐서를 PIL 이미지로 변환
+    ])
+    image = unnormalize(image_tensor.cpu())  # GPU에서 CPU로 이동 후 복원
+
+    # 2. 캡션 텍스트로 변환
+    caption = [vocab.idx2word[idx.item()] for idx in caption_tensor if idx.item() in vocab.idx2word]
+    caption_text = " ".join(caption)
+
+    image.save("./test.png")
+    with open("./out.txt", "w") as f:
+        f.write(caption_text)
+
+
+class RandomCaptionLengthSampler(sampler.Sampler):
+    def __init__(self, dataset, batch_size):
+        self.dataset = dataset
+        self.batch_size = batch_size
+
+        self.length_to_indices = {}
+        for idx, length in enumerate(self.dataset.caption_lengths):
+            if length not in self.length_to_indices:
+                self.length_to_indices[length] = []
+            self.length_to_indices[length].append(idx)
+
+    def __iter__(self):
+        """
+        랜덤한 길이를 선택하고, 해당 길이의 데이터로 배치를 생성.
+        """
+        lengths = list(self.length_to_indices.keys())
+
+        while True:
+            random_length = np.random.choice(lengths)  # 랜덤 길이 선택
+            all_indices = self.length_to_indices[random_length]
+
+            # 충분한 샘플이 있는 경우에만 처리
+            if len(all_indices) >= self.batch_size:
+                np.random.shuffle(all_indices)  # 인덱스를 무작위로 섞기
+                for i in range(0, len(all_indices), self.batch_size):
+                    batch = all_indices[i:i + self.batch_size]
+                    if len(batch) == self.batch_size:
+                        yield batch
+
+    def __len__(self):
+        return sum(len(indices) // self.batch_size for indices in self.length_to_indices.values())
+
+
+def train(device, epoch=1):
+    transform_train = transforms.Compose([
+        transforms.Resize(256),
+        transforms.RandomCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406),
+                             (0.229, 0.224, 0.225))])
+
+    transform_validation = transforms.Compose([
+        transforms.Resize(224),
+        transforms.ToTensor()
+    ])
+
+    train = COCODataset(transform_train,
+                       annotations_path="/Users/dsparch/Workspace/Data/COCO/annotations-2/captions_train2014.json",
+                       img_dir_path="/Users/dsparch/Workspace/Data/COCO/train2014",
+                       batch_size=32)
+
+    validation = COCODataset(transform_validation,
+                             annotations_path="/Users/dsparch/Workspace/Data/COCO/annotations-2/captions_val2014.json",
+                             batch_size=32,
+                             img_dir_path="/Users/dsparch/Workspace/Data/COCO/val2014")
+
+
+    data_loader = DataLoader(dataset=train,
+                             batch_sampler=RandomCaptionLengthSampler(train, 32),
+                             num_workers=4)
+
+    validation_data_loader = DataLoader(dataset=validation,
+                                        num_workers=4)
+
+    embed_size = 256
+    encoder = Encoder(embed_size)
+    decoder = Decoder(embed_size, 512, len(train.vocab))
+    model = ImageCaption(encoder, decoder)
+    model = model.to(device)
+    criterion = torch.nn.CrossEntropyLoss().to(device)
+    optimizer = torch.optim.Adam(model.parameters())
+
+    for epoch in range(1, epoch + 1):
+        for images, captions in data_loader:
+            #show_image_with_caption(images[0], captions[0], vocab=train.vocab)
+
+            images = images.to(device)
+            captions = captions.to(device)
+
+            outputs = model(images, captions)
+            print(outputs.size(), captions.size())
+            loss = criterion(
+                outputs.view(-1, len(train.vocab)),
+                captions[:, 1:].contiguous().view(-1)
+            )
+            loss.backward()
+            optimizer.step()
+            print(loss)
+
+        for images, captions in validation_data_loader:
+            pass
+
+
+if __name__ == "__main__":
+    device = "cpu"
+    if torch.cuda.is_available():
+        device = "cuda"
+    elif torch.backends.mps.is_available():
+        device = "mps"
+    train(device, 1)
